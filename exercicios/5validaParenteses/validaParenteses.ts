f8683677b92117d7a2962036b399e6dd81e0bd6b
Alfredo <gabriel94alfredo@gmail.com>
2024-01-03 20:32:32 -0300

    validando parenteses

---
+++ exercicios/5validaParenteses/validaParenteses.ts
@@ -0,0 +1,24 @@
+const s = '](';
+isValid(s);
+
+function isValid(s: string): boolean {
+  let parentesesAbertos: Array<string> = [];
+  if (s.length % 2 != 0 || s[0] === ')' || s[0] === ']' || s[0] === '}') {
+    return false;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+      parentesesAbertos.push(s[i]);
+    } else if (parentesesAbertos[parentesesAbertos.length - 1] + s[i] === '()') {
+      parentesesAbertos.pop();
+    } else if (parentesesAbertos[parentesesAbertos.length - 1] + s[i] === '[]') {
+      parentesesAbertos.pop();
+    } else if (parentesesAbertos[parentesesAbertos.length - 1] + s[i] === '{}') {
+      parentesesAbertos.pop();
+    } else return false;
+  }
+  if (parentesesAbertos.length > 0) {
+    return false;
+  }
+  return parentesesAbertos.length === 0;
+}
