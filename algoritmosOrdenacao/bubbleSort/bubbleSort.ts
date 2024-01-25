afaa26a4b9af3b295bde0627deaec3f2fa255bfc
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-01-25 16:40:47 -0300

    insertion sort

--- algoritmosOrdenacao/bubbleSort/bubbleSort.ts
+++ algoritmosOrdenacao/bubbleSort/bubbleSort.ts
@@ -1,16 +1,14 @@
-
 // complexidade O(n²)=> nao recomendado para uso para grande quantidade de dados
-const arr: number[] = [10, 5, 4, 4, 6, 7, 2, 8, 3, 0, 1]
+const arr: number[] = [10, 5, 4, 4, 6, 7, 2, 8, 3, 0, 1];
 
-
-bubbleSort(arr)
+bubbleSort(arr);
 function bubbleSort(arr: number[]) {
-  let ciclos:number=0
-  for (let i  = 0; i  < arr.length-1; i ++) {
-    for (let j = 0; j < arr.length - 1; j++){
-      ciclos++
+  let ciclos: number = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      ciclos++;
       if (arr[j] > arr[j + 1]) {
-        [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
--- algoritmosOrdenacao/insertionSort/insertionSort.ts
+++ algoritmosOrdenacao/insertionSort/insertionSort.ts
@@ -1,12 +1,18 @@
+// const numeros: number[] = [0, 1, 2, 3];
 const numeros: number[] = [9, 7, 5, 6, 3, 100, 101, 1, 0, 8, 9, 10, 12, 0, 78];
 // const numeros:number[]=[4,5,8,2,9,1,3,7,6]
 // const numeros:number[]=[9,8,7,6,5,4,3,2,1]
-
+let troca = 0;
+let ciclos = 0;
 for (let i = 1; i < numeros.length; i++) {
-  let atual:number=i
-  while (numeros[atual]<numeros[atual-1]&& atual!==0) {
-    [numeros[atual], numeros[atual - 1]] = [numeros[atual - 1], numeros[atual]]
-    atual--
+  ciclos++;
+  let atual: number = i;
+  while (numeros[atual] < numeros[atual - 1] && atual !== 0) {
+    [numeros[atual], numeros[atual - 1]] = [numeros[atual - 1], numeros[atual]];
+    atual--;
+    troca++;
+    ciclos++;
   }
 }
-console.log(numeros)
+ciclos;
+troca;
