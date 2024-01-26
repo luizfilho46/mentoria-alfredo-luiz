1f6d09b2401af8eac4219f69ecee94a0472ebc06
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-01-26 14:20:40 -0300

    desenvolvimento de solucao exercicio 147 leetcode

--- algoritmosOrdenacao/insertionSort/exercicios/insertionSortList.ts
+++ algoritmosOrdenacao/insertionSort/exercicios/insertionSortList.ts
@@ -10,10 +10,16 @@
  * }
  */
 
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+const lista=
+insertionSortList(lista)
 function insertionSortList(head: ListNode | null): ListNode | null {
-  
-  
-  
-  return head
-    
-};
+  return head;
+}
