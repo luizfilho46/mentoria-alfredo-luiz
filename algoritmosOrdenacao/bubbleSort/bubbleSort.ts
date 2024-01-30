fcdd69be5839251eadfa57c9f941ecda6f34686c
Alfredo <gabriel94alfredo@gmail.com>
2024-01-30 20:10:09 -0300

    Array de numeros aleatorios

--- algoritmosOrdenacao/bubbleSort/bubbleSort.ts
+++ algoritmosOrdenacao/bubbleSort/bubbleSort.ts
@@ -1,7 +1,7 @@
 // complexidade O(n²)=> nao recomendado para uso para grande quantidade de dados
-const arr: number[] = [10, 5, 4, 4, 6, 7, 2, 8, 3, 0, 1];
-
-bubbleSort(arr);
+// const arr: number[] = [10, 5, 4, 4, 6, 7, 2, 8, 3, 0, 1];
+import numeros from "../numerosAleatorios";
+bubbleSort(numeros);
 function bubbleSort(arr: number[]) {
   let ciclos: number = 0;
   for (let i = 0; i < arr.length - 1; i++) {
@@ -12,5 +12,5 @@ function bubbleSort(arr: number[]) {
       }
     }
   }
-  console.log(`${arr} ciclos ${ciclos}`);
+  console.log(`array ${arr} ciclos ${ciclos}`);
 }
