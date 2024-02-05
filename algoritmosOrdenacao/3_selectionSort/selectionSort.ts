f48c3da0ea5fff662497d93dee015cd2f3b4bff1
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-02-05 14:38:21 -0300

    algoritmo insertion sort

---
+++ algoritmosOrdenacao/3_selectionSort/selectionSort.ts
@@ -0,0 +1,19 @@
+import numeros from '../numerosAleatorios';
+
+function selectionSort(arr: Array<number>): Array<number> {
+  let menor: number;
+  for (let i = 0; i < arr.length - 1; i++) {
+    menor = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[menor]) {
+        menor = j;
+      }
+    }
+    if (i !== menor) {
+      [arr[i], arr[menor]] = [arr[menor], arr[i]];
+    }
+  }
+  return arr;
+}
+
+selectionSort(numeros);
