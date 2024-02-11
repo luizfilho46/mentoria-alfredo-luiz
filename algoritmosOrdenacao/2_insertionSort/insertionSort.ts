970a4f3bda1047167e0f80e66e0a970052d904dd
Alfredo <gabriel94alfredo@gmail.com>
2024-02-11 09:26:37 -0300

    Inserido url de referencia

--- algoritmosOrdenacao/2_insertionSort/insertionSort.ts
+++ algoritmosOrdenacao/2_insertionSort/insertionSort.ts
@@ -1,18 +1,16 @@
-// const numeros: number[] = [0, 1, 2, 3];
 const numeros: number[] = [9, 7, 5, 6, 3, 100, 101, 1, 0, 8, 9, 10, 12, 0, 78];
-// const numeros:number[]=[4,5,8,2,9,1,3,7,6]
-// const numeros:number[]=[9,8,7,6,5,4,3,2,1]
-let troca = 0;
-let ciclos = 0;
-for (let i = 1; i < numeros.length; i++) {
-  ciclos++;
-  let atual: number = i;
-  while (numeros[atual] < numeros[atual - 1] && atual !== 0) {
-    [numeros[atual], numeros[atual - 1]] = [numeros[atual - 1], numeros[atual]];
-    atual--;
-    troca++;
-    ciclos++;
+function InsertionSort(vetor: Array<number>) {
+  let atual;
+  for (let i = 0; i < vetor.length; i++) {
+    let j = i - 1; //ultima posicao ordenada
+    atual = vetor[i];
+    while (j >= 0 && atual < vetor[j]) {
+      vetor[j + 1] = vetor[j];
+      j--;
+    }
+    vetor[j + 1] = atual;
   }
+  return vetor;
 }
-ciclos;
-troca;
+
+// video de referencia: https://www.youtube.com/watch?v=vXOzjDX69PM
