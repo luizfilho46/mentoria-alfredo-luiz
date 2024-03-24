0086bb9eb6de1d54f88687a2aeefbcb69ce0b032
Alfredo <gabriel94alfredo@gmail.com>
2024-03-24 00:04:10 -0300

    codigo refatorado

--- exercicios/4_medianTwoSortedArrays/medianTwoSortedArrays.ts
+++ exercicios/4_medianTwoSortedArrays/medianTwoSortedArrays.ts
@@ -1,29 +1,23 @@
-const nums1 = [0, 0, 0, 0];
-const nums2 = [1];
+const nums1 = [0];
+const nums2 = [0];
 
-findMedianSortedArrays(nums1, nums2);
+console.log(findMedianSortedArrays(nums1, nums2));
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-  let tamanhoArr = nums1.length + nums2.length;
-  let arrFinal = [];
-  let position1 = 0,
-    position2 = 0;
-  while (arrFinal.length < tamanhoArr) {
-    if (nums1[position1] <= nums2[position2]) {
-      arrFinal.push(nums1[position1]);
-      position1++;
-    } else if (nums1[position1] > nums2[position2]) {
-      arrFinal.push(nums2[position2]);
-      position2++;
-    } else if (nums1[position1] !== undefined && nums2[position2] === undefined) {
-      arrFinal.push(nums1[position1]);
-      position1++;
-    } else if (nums1[position1] === undefined && nums2[position2] !== undefined) {
-      arrFinal.push(nums2[position2]);
-      position2++;
+  const arrFinal = [];
+  let positionNums1 = 0;
+  let positionNums2 = 0;
+  while (arrFinal.length < nums1.length + nums2.length) {
+    if (nums1[positionNums1] <= nums2[positionNums2] || nums2[positionNums2] === undefined) {
+      arrFinal.push(nums1[positionNums1]);
+      positionNums1++;
+    } else {
+      arrFinal.push(nums2[positionNums2]);
+      positionNums2++;
     }
   }
-  let indice = Math.floor(arrFinal.length / 2);
+  let meioArrFinal = Math.floor(arrFinal.length / 2);
   if (arrFinal.length % 2 === 0) {
-    return (arrFinal[indice - 1] + arrFinal[indice]) / 2;
-  } else return arrFinal[indice];
+    return (arrFinal[meioArrFinal - 1] + arrFinal[meioArrFinal]) / 2;
+  }
+  return arrFinal[meioArrFinal];
 }
