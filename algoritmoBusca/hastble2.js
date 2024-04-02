5c13a115142d4406eab5c4789aaa8fca975ca2e0
al-lourenco <gabriel94alfredo@gmail.com>
2024-04-01 22:46:17 -0300

    remove elemento

--- algoritmoBusca/hastble2.js
+++ algoritmoBusca/hastble2.js
@@ -1,1 +1,35 @@
-const table=new Array()
+class HasTable {
+  hash;
+  constructor(value) {
+    this.hash = new Array(value);
+  }
+
+  addValue(value) {
+    let lengthTable = this.hash.length - 1;
+    let cycles = value % lengthTable;
+    while (cycles <= lengthTable && this.hash[cycles] !== undefined && this.hash[cycles] !== null) {
+      cycles++;
+    }
+    if (cycles < lengthTable) {
+      this.hash[cycles] = value;
+    }
+  }
+  removeElement(value) {
+    let lengthTable = this.hash.length - 1;
+    let cycles = value % lengthTable;
+    while (cycles <= lengthTable && this.hash[cycles] !== undefined && this.hash[cycles] !== null) {
+      cycles++;
+    }
+    if (this.hash[cycles] === value) {
+      this.hash[cycles] = null;
+    }
+  }
+}
+const hash = new HasTable(2003);
+
+hash.addValue(2003);
+hash.addValue(0);
+hash.addValue(2003);
+hash.addValue(10);
+
+console.log(hash);
