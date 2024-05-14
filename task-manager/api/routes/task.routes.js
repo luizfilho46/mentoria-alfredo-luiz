0a0fc8ef1b4a49ac46c0d62256077c25d5a32fec
al-lourenco <gabriel94alfredo@gmail.com>
2024-05-14 00:35:27 -0300

    validate edit endpoint

--- task-manager/api/routes/task.routes.js
+++ task-manager/api/routes/task.routes.js
@@ -5,5 +5,5 @@ const router = require('express').Router();
 router.post('/', validaTask, taskControllers.create);
 router.get('/', taskControllers.get);
 router.delete('/:id', taskControllers.delete);
-router.put('/:id', taskControllers.edit);
+router.put('/:id', validaTask, taskControllers.edit);
 module.exports = router;
