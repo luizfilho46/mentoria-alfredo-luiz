8710f6c24ab94a96a640e28cd1f293f35c1cc3cd
al-lourenco <gabriel94alfredo@gmail.com>
2024-05-14 00:10:31 -0300

    hastable em ts

--- algoritmoBusca/hash.ts
+++ algoritmoBusca/hash.ts
@@ -1,10 +1,20 @@
 class Hash<Number> {
-  hash: number[];
+  private hash: number[];
+  private tamanho: number;
   constructor(value: number) {
     this.hash = new Array(value);
+    this.tamanho = value;
+  }
+  adiconar(value: number) {
+    const indice = value % this.tamanho;
+    this.hash[indice] = value;
+  }
+  buscar(value: number) {
+    const indice = value % this.tamanho;
+    return this.hash[indice];
   }
 }
 
 const hash = new Hash<number>(2003);
-
-console.log(hash)
+hash.adiconar(10);
+console.log(hash.buscar(10));
