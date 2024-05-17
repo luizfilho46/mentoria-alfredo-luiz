304651fbac1037f13f2ecb23990d7464035a25b3
al-lourenco <gabriel94alfredo@gmail.com>
2024-05-17 15:13:46 -0300

    correcao de indentacao

--- task-manager/api/middleware/validaTasks.js
+++ task-manager/api/middleware/validaTasks.js
@@ -2,17 +2,16 @@ const { validaDescricao, isValidDate, verificaDataInicial, verificaMaiorData } =
 
 const validaTask = (req, res, next) => {
   const { descricao, dataInicio, dataFim } = req.body;
-try {
-  validaDescricao(descricao);
-  isValidDate(dataInicio);
-  isValidDate(dataFim);
-  verificaDataInicial(dataInicio);
-  verificaMaiorData(dataInicio, dataFim);
-  next();
-} catch (error) {
-  res.status(500).json({msg:error})
-}
-  
+  try {
+    validaDescricao(descricao);
+    isValidDate(dataInicio);
+    isValidDate(dataFim);
+    verificaDataInicial(dataInicio);
+    verificaMaiorData(dataInicio, dataFim);
+    next();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = validaTask;
