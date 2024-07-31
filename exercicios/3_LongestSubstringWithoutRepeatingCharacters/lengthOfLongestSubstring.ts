4c595eefbad68f82935d3dec4faca4876600a4b2
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-07-31 17:07:59 -0300

    length Of Longest Substring

--- exercicios/3_LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring.ts
+++ exercicios/3_LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring.ts
@@ -1,30 +1,13 @@
-const s1 = 'abcabcbb'; // 3
-const s2 = 'bbbbb'; // 1
-const s3 = 'pwwkew'; // 3
-const s4 = 'p'; // 1
-const s5 = ''; // 0
-const s6 = 'dvdf'; // 3
-
 function lengthOfLongestSubstring(s: string): number {
-  let current: string = '';
   let max: number = 0;
-  let restart: number = 0;
-  for (let i = restart; i < s.length; i++) {
-    if (!current.includes(s[i])) {
-      current += s[i];
-    } else {
-      if (max < current.length) {
-        max = current.length;
-      }
-      current = '';
-      i = restart++;
+  for (let i = 0; i < s.length; i++) {
+    let currentSubstring = new Set();
+    for (let j = i; j < s.length; j++) {
+      if (!currentSubstring.has(s[j])) currentSubstring.add(s[j]);
+      else break;
     }
+    if (currentSubstring.size > max) max = currentSubstring.size;
+    if (max === s.length || max > s.slice(i).length) break;
   }
-  return max > current.length ? max : current.length;
+  return max;
 }
-console.log(lengthOfLongestSubstring(s1));
-// console.log(lengthOfLongestSubstring(s2));
-// console.log(lengthOfLongestSubstring(s3));
-// console.log(lengthOfLongestSubstring(s4));
-// console.log(lengthOfLongestSubstring(s5));
-// console.log(lengthOfLongestSubstring(s6));
